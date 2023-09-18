b79efc253eace8fdcf5fb7e78c9d40da0efc4882
黃韻儒 <lalame888@gmail.com>
2023-09-18 13:35:37 +0800

    fix: 切分module fix: 將cookie從config中抽出，cookie中若有 % 符號會造成無法從config檔讀入

--- itHelp.py
+++ itHelp.py
@@ -1,33 +1,11 @@
-import requests
-from bs4 import BeautifulSoup
-import re
-from urllib.parse import urlencode
 from datetime import datetime
 import schedule
 import time
 import threading
-from configparser import ConfigParser
+import utils as module
 
-config = ConfigParser()
-config.read('itHelpConfig.ini')
-cookie_value = config.get('Settings', 'cookie_value')
-lineToken = config.get('Settings', 'line_token')
-
+cookie_value = '_ga=GA1.1.2000627303.1694786881; token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL21lbWJlci5pdGhvbWUuY29tLnR3L2xvZ2luIiwiaWF0IjoxNjk0Nzk3OTAyLCJleHAiOjE2OTczODk5MDIsIm5iZiI6MTY5NDc5NzkwMiwianRpIjoiSTFhcXpTMFFPTDRuVllZRiIsInN1YiI6IjE4ODg2MSIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjciLCJjZW50ZXJfaWQiOjE4ODg2MSwiYWNjb3VudCI6InMxMDUwNTMwMDEiLCJlbWFpbCI6InMxMDUwNTMwMDFAc21haWwubmNodS5lZHUudHciLCJuaWNrbmFtZSI6InMxMDUwNTMwMDEiLCJjb21wYW55X25hbWUiOm51bGwsInVwZGF0ZWRfYXQiOjE2OTQ3OTc5MDF9.wdwpcX9zfOQl8v6dYp7CqiFO_Q7YOkhkJJCjN3qBfk0; _ga_6182VBHV8Y=GS1.1.1695006514.14.1.1695006571.0.0.0; XSRF-TOKEN=eyJpdiI6Ik04aHlRN1VGSVE2MmlJTFkrTldWcFE9PSIsInZhbHVlIjoiQzBYcGtncElNenM0U2VlK0NWZVFhS2hsWkxqSTFRNm95NVl1Y1l0OEsxWVJQNUZncU9WS1dCRGxPRFArTEtmMyIsIm1hYyI6ImVlYjU4NDM1MWZmZGRlYmU2NTEyNDQwYzk0MGQ3ZTg3Y2EyOTEzZmI1MmJiYTkzOGY5OThmOWUzYWRkZjU2OGEifQ%3D%3D; ithelp2016_desktop=eyJpdiI6IkFwT29Days0bEd4YTFiOEptTHJYMXc9PSIsInZhbHVlIjoiVFZUdzFUV2F0aEN0bEd1M21BcGY0THpnWjhjOFE5Y2N3cVVPbW1HNEVlXC96MWtKRG5ETCtmUU0yNVlsVmdWaHQiLCJtYWMiOiJkNTQ4ZjkyNjViMWEyNjhiZGU0MjllYzBiYmI0MTQyNjE2NzFhNTNlODA1OThhZjZmNWU3Mzc0OGZmNmJjODllIn0%3D'
 stop_event = threading.Event()
-
-# 建立session
-def getSession(cookie_value):
-    session = requests.Session()
-    # 設定 User-Agent 標頭
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    }
-    # 將 Cookie 存儲在 Session 中
-    session.headers.update(headers)
-    session.cookies['Cookie'] = cookie_value
-    return session
-session = getSession(cookie_value)
 
 def is_valid_time(input_str):
     try:
@@ -36,177 +14,34 @@ def is_valid_time(input_str):
     except ValueError:
         return False
 
-# 取得IT邦幫忙的網站內容
-def scrape_website(url, method = 'GET', data = {}, session = session):
-    if method.upper() == 'GET':
-        response = session.get(url)
-    else :
-        response = session.post(url, data=data, json=data)
-    # 檢查請求的回應
-    if response.status_code == 200:
-        # 處理回應內容
-        return (response.text)
-    else:
-        raise Exception(f'請求失敗，狀態碼：{response.status_code}, 錯誤訊息：{response.text}')
-
-#發送到line通知    
-def line_notify(message):
-    try:
-        url = 'https://notify-api.line.me/api/notify'
-        data = { 'message': message }
-        lineSession = getSession('')
-        lineSession.headers = {"Authorization": f"Bearer {lineToken}"}
-        lineSession.headers.update(lineSession.headers)
-        scrape_website(url, 'POST', data, lineSession)
-    except Exception as error:
-        print(f'串接line失敗 : error')
-
 def no_login_alter():
-    line_notify('目前登入Tonke過期，請重新設定Token')
-
-
-# 取得使用者id
-def getUser():
-    isLogin = False
-    name = ''
-    id = ''
-    url = 'https://ithelp.ithome.com.tw/'
-    page_content = scrape_website(url)
-    soup = BeautifulSoup(page_content, 'html.parser')
-    account = soup.find('a', {'id': 'account'})
-    if account :
-        isLogin = True
-        name = account['data-account']
-        href = account['href']
-        id = re.search(r'users/(\d+)', href).group(1)
-    return { 'isLogin': isLogin, 'name': name , 'id': id }
-
-
-# 取得草稿列表 Array<{link:string, text: string, id:string}>
-def getArticlesList(userId):
-    url = f'https://ithelp.ithome.com.tw/users/{userId}/articles'
-    page_content = scrape_website(url)
-    soup = BeautifulSoup(page_content, 'html.parser')
-    div_elements = soup.find_all('div', class_='qa-list profile-list')
-
-    if div_elements:
-        # 遍歷所有找到的元素，並處理它們
-        # 使用列表推導式篩選出包含 <span class="title-badge title-badge--draft"> 的 <div> 元素
-        filtered_div_elements = [div_element for div_element in div_elements if div_element.find('span', class_='title-badge title-badge--draft')]
-        # 檢查篩選後的結果
-        results = []  # 創建一個空的物件陣列來存儲結果
-        if filtered_div_elements:
-            for  list_element in filtered_div_elements:
-                title_link = list_element.find('a', class_='qa-list__title-link')
-                link = title_link.get('href').strip()
-                text = title_link.text.strip()
-                id = re.search(r'articles/(\d+)', link).group(1)
-                result_dict = {'link': link, 'text': text, 'id': id}
-                results.append(result_dict)
-        results.reverse()
-        return results
-    else:
-        return []
-
-# 取得文章的發文token
-def getDraftContent(id):
-    url = f'https://ithelp.ithome.com.tw/articles/{id}/draft'
-    page_content = scrape_website(url)
-    soup = BeautifulSoup(page_content, 'html.parser')
-    input_element = soup.find('input', {'name': '_token'})
-    if input_element == None:
-        raise Exception(f'發文token取得失敗')
-    token = input_element['value']
-    subject = soup.find('input', {'name': 'subject'})['value']
-    description =  soup.find('textarea', {'name': 'description'}).text
-    tag_elements = soup.find('select', {'name': 'tags[]'}).find_all('option', selected='selected')
-    tags = [option.text for option in tag_elements]
-    return { 
-            'token': token,
-            'description': description,
-            'subject': subject,
-            'tags': tags
-        }
+    module.line_notify('目前登入Token過期，請重新設定Token')
 
 def showList(list):
     for index, element in enumerate(list, start=1):
         print(f"{index}. {element['text']}")
 
-def publish(id):
-    global session
-    draftContent = getDraftContent(id)
-    url = f'https://ithelp.ithome.com.tw/articles/{id}/publish'
-
-    newHeaders = session.headers
-    newHeaders['X-Csrf-Token'] = draftContent['token']
-    newHeaders['Referer'] = url
-    newHeaders['Origin'] = 'https://ithelp.ithome.com.tw'
-    session.headers.update(newHeaders)
-    subject = draftContent['subject']
-    data = {'_token': draftContent['token'],
-         'group': 'tech',
-         '_method': 'PUT',
-         'subject': subject,
-         'description': draftContent['description'],
-         'tags[]': draftContent['tags'],
-        }
-    # 將tags[]的值編碼
-    encoded_data = urlencode(data, doseq=True)
-    scrape_website(url, 'POST', encoded_data)
-    message = f'已發布文章！ 文章名稱: {subject} ; id: {id}'
-    line_notify(message)
-    print(message)
-
-def showDraft(userId):
-    draftList = getArticlesList(userId)
+def showDraft(session):
+    draftList = module.getArticlesList(session)
     length = len(draftList)
     print(f'＊以下是目前尚未發布的草稿文章列表: 共 {length} 篇＊')
 
     showList(draftList)
     print('')
 
-def login(loginId, ps):
-    # 去登入看看
-    global session
-    global cookie_value
-    url = 'https://member.ithome.com.tw/login'
-    loingPage = scrape_website(url)
-    soup = BeautifulSoup(loingPage, 'html.parser')
-    token = soup.find('input', {'name': '_token'})['value']
-
-    newHeaders = session.headers
-    newHeaders['X-Csrf-Token'] = token
-    newHeaders['Referer'] = url
-    newHeaders['Origin'] = 'https://member.ithome.com.tw'
-    # 將 Cookie 存儲在 Session 中
-    session.headers.update(newHeaders)
-    data = {'_token': token, 'account': loginId, 'password': ps}
-    response = session.post(url, data=data)
-     # 檢查請求的回應
-    if response.status_code == 200:
-        # 處理回應內容
-        # if (response.cookies):
-        #     RequestsCookieJar = response.cookies.values()
-        #     cookie_value = RequestsCookieJar[0]
-        #     session = getSession(cookie_value)
-        #     print(f'設定cookie_value = {cookie_value}')
-        print (response.text)
-    else:
-        raise Exception(f'請求失敗，狀態碼：{response.status_code}, 錯誤訊息：{response.text}')
-
-def postLast(userId):
-    draftList = getArticlesList(userId)
+def postLast(session):
+    draftList = module.getArticlesList(session)
     if len(draftList) > 0 :
         postId = draftList[0]['id']
-        publish(postId)
-        showDraft(userId)
+        module.publish(session, postId)
+        showDraft(session)
     else: 
         message = '發文失敗，目前無草稿文章'
-        line_notify(message)
+        module.line_notify(message)
         print(message) 
 
-def chosePost(userId):
-    draftList = getArticlesList(userId)
+def chosePost(session):
+    draftList = module.getArticlesList(session)
     print('目前的草稿文章清單：')
     showList(draftList)
     while True:
@@ -215,7 +50,7 @@ def chosePost(userId):
             if postIndex == 'exit':
                 break
             elif int(postIndex) <= len(draftList):
-                publish(draftList[int(postIndex) - 1]['id'])
+                module.publish(session, draftList[int(postIndex) - 1]['id'])
                 break
             else :
                 print('輸入無效，請重新選擇')
@@ -223,13 +58,13 @@ def chosePost(userId):
             print(error)
             print('發生錯誤，請重新選擇')
 
-def autoPostFunction(open, time, userId):
+def autoPostFunction(open, time, session):
     schedule.cancel_job(postLast)
     if open:
-        schedule.every().day.at(time).do(postLast, userId)
-        line_notify(f'開啟自動發文，時間為{time}')
+        schedule.every().day.at(time).do(postLast, session)
+        module.line_notify(f'開啟自動發文，時間為{time}')
     else:
-        line_notify(f'關閉自動發文功能')
+        module.line_notify(f'關閉自動發文功能')
 
 
 def run_schedule():
@@ -239,87 +74,90 @@ def run_schedule():
         # print('check!')
 
 def main():
-    autoPost = True
+    global cookie_value
+    autoPost = False
     autoPostTime = '10:00:00'
     schedule_thread = threading.Thread(target=run_schedule)
     schedule_thread.start()
-    line_notify(f'\n啟動IT邦幫忙自動發文神器\n自動發文設定為: {"開，設定時間為每日 " + autoPostTime if autoPost else "關"}')
-    user = getUser()
-    if user['isLogin']:
+    session = module.getSession(cookie_value)
+    module.line_notify(f'\n啟動IT邦幫忙自動發文神器\n自動發文設定為: {"開，設定時間為每日 " + autoPostTime if autoPost else "關"}')
+    user = module.getUser(session)
+    if user:
         userName = user['name']
-        line_notify(f'目前登入帳號為: {userName}')
+        module.line_notify(f'目前登入帳號為: {userName}')
     try:
         while True :
-                user = getUser()
-                userId = user['id']
-                userName = user['name']
-                schedule.cancel_job(no_login_alter)
-                if user['isLogin']:
-                    print(f'目前登入帳號: {userName}')
-                    message = f'自動發文設定為: {"開，設定時間為每日 " + autoPostTime if autoPost else "關"}'
-                    print(message)
-                    print("--------- 選單 ---------")
-                    print("1. 檢視未發布草稿文章列表")
-                    print('2. 發布最早儲存的草稿')
-                    print("3. 選擇文章發布")
-                    print(f'4. {"關閉" if autoPost else "開啟"} 自動定時發布')
-                    print("5. 修改每日定時發布時間")
+            session = module.getSession(cookie_value)
+            user = module.getUser(session)
+            userId = user['id']
+            userName = user['name']
+            schedule.cancel_job(no_login_alter)
+            if user:
+                print(f'目前登入帳號: {userName}')
+                message = f'自動發文設定為: {"開，設定時間為每日 " + autoPostTime if autoPost else "關"}'
+                print(message)
+                print("--------- 選單 ---------")
+                print("1. 檢視未發布草稿文章列表")
+                print('2. 發布最早儲存的草稿')
+                print("3. 選擇文章發布")
+                print(f'4. {"關閉" if autoPost else "開啟"} 自動定時發布')
+                print("5. 修改每日定時發布時間")
 
-
-                    print("或是輸入 exit 關閉離開")
-                    print("")
-                    choice = input("請輸入您的選擇：")
-                        # 根據使用者的選擇執行相應的操作
-                    if choice == "1":
-                        showDraft(userId)
-                    elif choice == "2":
-                        postLast(userId)          
-                    elif choice == "3":
-                        chosePost(userId)
-                    elif choice == '4':
-                        autoPost = not autoPost
-                        autoPostFunction(autoPost, autoPostTime, userId)
-                    elif choice == '5':
-                        print(f'目前設定時間為每日 {autoPostTime}')
-                        while True:  
-                            if autoPost:
-                                time_str = input("請輸入時間（格式為HH:MM:SS），或是exit取消：")
-                                if time_str == 'exit':
-                                    break
-                                if is_valid_time(time_str):
-                                    autoPostTime = datetime.strptime(time_str, '%H:%M:%S').strftime('%H:%M:%S') # 以24小時制的格式進行解析
-                                    autoPostFunction(True, autoPostTime, userId)
-                                    print(f'已設定為{autoPostTime}')
-                                    break
-                                else:
-                                    print(f"{time_str} 不是有效的時間格式")
+                print("或是輸入 exit 關閉離開")
+                print("")
+                choice = input("請輸入您的選擇：")
+                    # 根據使用者的選擇執行相應的操作
+                if choice == "1":
+                    showDraft(session)
+                elif choice == "2":
+                    postLast(session)          
+                elif choice == "3":
+                    chosePost(session)
+                elif choice == '4':
+                    autoPost = not autoPost
+                    autoPostFunction(autoPost, autoPostTime, session)
+                elif choice == '5':
+                    print(f'目前設定時間為每日 {autoPostTime}')
+                    while True:  
+                        if autoPost:
+                            time_str = input("請輸入時間（格式為HH:MM:SS），或是exit取消：")
+                            if time_str == 'exit':
+                                break
+                            if is_valid_time(time_str):
+                                autoPostTime = datetime.strptime(time_str, '%H:%M:%S').strftime('%H:%M:%S') # 以24小時制的格式進行解析
+                                autoPostFunction(True, autoPostTime, session)
+                                print(f'已設定為{autoPostTime}')
+                                break
                             else:
-                                newInput = input('目前尚未開啟自動發文，是否要先開啟？(y/n)')
-                                if (newInput.upper() == 'Y'):
-                                    autoPost = True
-                                    autoPostFunction(True, autoPostTime, userId)
-                                else:
-                                    print('退出時間設定')
-                                    break
-                    elif choice == "exit":
-                        print("ㄅㄅ！")
-                        break  # 選擇離開時結束循環
-                    else:
-                        print("無效的選擇，請重新輸入。")
-                    print('')
+                                print(f"{time_str} 不是有效的時間格式")
+                        else:
+                            newInput = input('目前尚未開啟自動發文，是否要先開啟？(y/n)')
+                            if (newInput.upper() == 'Y'):
+                                autoPost = True
+                                autoPostFunction(True, autoPostTime, session)
+                            else:
+                                print('退出時間設定')
+                                break
+
+                elif choice == "exit":
+                    print("ㄅㄅ！")
+                    break  # 選擇離開時結束循環
                 else:
-                    schedule.every(30).minutes.do(no_login_alter)
-                    loginId = input("目前登入cookie無效，請重新輸入登入帳號: ")
-                    ps = input("請輸入密碼:")
-                    print('登入中...')
-                    login(loginId, ps)
+                    print("無效的選擇，請重新輸入。")
+                print('')
+            else:
+                schedule.every(30).minutes.do(no_login_alter)
+                loginId = input("目前登入cookie無效，請重新輸入登入帳號: ")
+                ps = input("請輸入密碼:")
+                print('登入中...')
+                cookie_value = module.login(loginId, ps)
     except Exception as error:
-        line_notify(f'發生錯誤意外關閉: {error}')
+        module.line_notify(f'發生錯誤意外關閉: {error}')
         print(error)
         stop_event.set()
     stop_event.set()
     schedule_thread.join()
-    line_notify('關閉IT邦幫忙發文神器')
+    module.line_notify('關閉IT邦幫忙發文神器')
 
 
 main()
---
+++ utils.py
@@ -0,0 +1,185 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from urllib.parse import urlencode
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('itHelpConfig.ini')
+lineToken = config.get('Settings', 'line_token')
+
+# 建立session
+def getSession(cookie_value = ''):
+    session = requests.Session()
+    # 設定 User-Agent 標頭
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    }
+    # 將 Cookie 存儲在 Session 中
+    session.headers.update(headers)
+    session.cookies['Cookie'] = cookie_value
+    return session
+
+# 發出request取得網站內容或回應
+def scrape_website(session, url, method = 'GET', data = {}):
+    if method.upper() == 'GET':
+        response = session.get(url)
+    else :
+        response = session.post(url, data=data, json=data)
+    # 檢查請求的回應
+    if response.status_code == 200:
+        # 處理回應內容
+        return (response.text)
+    else:
+        raise Exception(f'請求失敗，狀態碼：{response.status_code}, 錯誤訊息：{response.text}')
+
+#發送到line通知    
+def line_notify(message):
+    return
+    try:
+        url = 'https://notify-api.line.me/api/notify'
+        data = { 'message': message }
+        lineSession = getSession('')
+        lineSession.headers = {"Authorization": f"Bearer {lineToken}"}
+        lineSession.headers.update(lineSession.headers)
+        scrape_website(lineSession, url, 'POST', data)
+    except Exception as error:
+        print(f'串接line失敗 : error')
+
+
+# 取得使用者{id: string, name: string} | None 
+def getUser(session):
+    url = 'https://ithelp.ithome.com.tw/'
+    page_content = scrape_website(session, url)
+
+    soup = BeautifulSoup(page_content, 'html.parser')
+    account = soup.find('a', {'id': 'account'})
+    if account :
+        name = account['data-account']
+        href = account['href']
+        id = re.search(r'users/(\d+)', href).group(1)
+        return { 'name': name , 'id': id }
+    else:
+        return None
+def editDraft(session, articlesId, Content):
+    draftContent = getDraftContent(session, articlesId)
+    url = f'https://ithelp.ithome.com.tw/articles/{articlesId}/draft'
+    token = draftContent['token']
+    newHeaders = session.headers
+    newHeaders['X-Csrf-Token'] = token
+    newHeaders['Origin'] = 'https://ithelp.ithome.com.tw'
+    session.headers.update(newHeaders)
+    subject = Content['subject']
+    data = {'_token': token,
+         'group': 'tech',
+         '_method': 'PUT',
+         'subject': subject,
+         'description': Content['description'],
+         'tags[]': Content['tags'],
+        }
+    # 將tags[]的值編碼
+    encoded_data = urlencode(data, doseq=True)
+    scrape_website(session, url, 'POST', encoded_data)
+
+
+# 取得草稿列表 Array<{link:string, text: string, id:string}>
+def getArticlesList(session):
+    user = getUser(session)
+    if user == None:
+        raise Exception('登入狀態過期，無法取得文章列表')
+    userId = user['id']
+    url = f'https://ithelp.ithome.com.tw/users/{userId}/articles'
+
+    page_content = scrape_website(session, url)
+    soup = BeautifulSoup(page_content, 'html.parser')
+    div_elements = soup.find_all('div', class_='qa-list profile-list')
+
+    if div_elements:
+        # 遍歷所有找到的元素，並處理它們
+        # 使用列表推導式篩選出包含 <span class="title-badge title-badge--draft"> 的 <div> 元素
+        filtered_div_elements = [div_element for div_element in div_elements if div_element.find('span', class_='title-badge title-badge--draft')]
+        # 檢查篩選後的結果
+        results = []  # 創建一個空的物件陣列來存儲結果
+        if filtered_div_elements:
+            for  list_element in filtered_div_elements:
+                title_link = list_element.find('a', class_='qa-list__title-link')
+                link = title_link.get('href').strip()
+                text = title_link.text.strip()
+                id = re.search(r'articles/(\d+)', link).group(1)
+                result_dict = {'link': link, 'text': text, 'id': id}
+                results.append(result_dict)
+        results.reverse()
+        return results
+    else:
+        return []
+
+# 取得指定草稿文章的資料內容
+def getDraftContent(session, articlesId):
+    url = f'https://ithelp.ithome.com.tw/articles/{articlesId}/draft'
+    page_content = scrape_website(session, url)
+    soup = BeautifulSoup(page_content, 'html.parser')
+    input_element = soup.find('input', {'name': '_token'})
+    if input_element == None:
+        raise Exception(f'發文token取得失敗')
+    token = input_element['value']
+    subject = soup.find('input', {'name': 'subject'})['value']
+    description =  soup.find('textarea', {'name': 'description'}).text
+    tag_elements = soup.find('select', {'name': 'tags[]'}).find_all('option', selected='selected')
+    tags = [option.text for option in tag_elements]
+    return { 
+            'token': token,
+            'description': description,
+            'subject': subject,
+            'tags': tags
+        }
+
+def publish(session, articlesId):
+    draftContent = getDraftContent(session, articlesId)
+    url = f'https://ithelp.ithome.com.tw/articles/{articlesId}/publish'
+
+    newHeaders = session.headers
+    newHeaders['X-Csrf-Token'] = draftContent['token']
+    newHeaders['Origin'] = 'https://ithelp.ithome.com.tw'
+    session.headers.update(newHeaders)
+    subject = draftContent['subject']
+    data = {'_token': draftContent['token'],
+         'group': 'tech',
+         '_method': 'PUT',
+         'subject': subject,
+         'description': draftContent['description'],
+         'tags[]': draftContent['tags'],
+        }
+    # 將tags[]的值編碼
+    encoded_data = urlencode(data, doseq=True)
+    scrape_website(session, url, 'POST', encoded_data)
+    message = f'已發布文章！ 文章名稱: {subject} ; id: {articlesId}'
+    line_notify(message)
+    print(message)
+
+
+def login(loginId, ps):
+    # 去登入看看
+    session = getSession()
+    url = 'https://member.ithome.com.tw/login'
+    loginPage = scrape_website(session, url)
+    soup = BeautifulSoup(loginPage, 'html.parser')
+    token = soup.find('input', {'name': '_token'})['value']
+
+    newHeaders = session.headers
+    newHeaders['X-Csrf-Token'] = token
+    newHeaders['Origin'] = 'https://member.ithome.com.tw'
+    # 將 Cookie 存儲在 Session 中
+    session.headers.update(newHeaders)
+    data = {'_token': token, 'account': loginId, 'password': ps}
+    response = session.post(url, data=data)
+     # 檢查請求的回應
+    if response.status_code == 200:
+        # 處理回應內容
+        if (response.cookies):
+            return requests.utils.dict_from_cookiejar(response.cookies)
+    else:
+        raise Exception(f'請求失敗，狀態碼：{response.status_code}, 錯誤訊息：{response.text}')
+
+
+# 作者： 一宵三筵 (lalame888)
